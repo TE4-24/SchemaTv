1b8a65be72f546cba82183c1a213527515a20ebb
sammar <148446217+Sammar99@users.noreply.github.com>
2024-09-03 15:42:47 +0200

    big change

--- index.php
+++ index.php
@@ -1,20 +1,36 @@
 <html lang="en">
 
 <head>
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 
 <body>
+  <?php
+
+  echo "<div class='header'>Schema</div>";
+
+  echo "<div class='year-container'>";
+
+  echo "<div class='class-schema-container'>
+        <div class='ee22'><h1 class='klass-namn'>EE22</h1></div>
+        <div class='schema'>schema</div>
+      </div>";
+
+  echo "<div class='class-schema-container'>
+        <div class='es22'><h1 class='klass-namn'>ES22</h1></div>
+        <div class='schema'>schema</div>
+      </div>";
+
+  echo "<div class='class-schema-container'>
+        <div class='te22'><h1 class='klass-namn'>TE22</h1></div>
+        <div class='schema'>schema</div>
+      </div>";
+
+  echo "</div>";
 
 
-    <a href="admin.php" class="btn">Admin</a>
-
-    <a href="about.php" class="btn">About</a>
-
-
-    <?php
-    ?>
+  ?>
 
 </body>
 
