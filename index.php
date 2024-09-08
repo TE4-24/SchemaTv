f55852251a92f60d05c7fe7752cde362994957af
Marcus <marnor1018@gmail.com>
2024-09-08 21:18:21 +0200

    daily schedule for 3 classes

--- index.php
+++ index.php
@@ -23,28 +23,36 @@
         <div class='te22'><h1 class='klass-namn'>TE22</h1></div>
         <div class='schema'>";
 */
-  // open the CSV file
-  if (($csvHandle = fopen("class_schedules/TE22.csv", "r")) !== FALSE) {
-    $currentDay = array();
-    while (($scheduleDays = fgetcsv($csvHandle, 1000, ",")) !== FALSE) {
-      $currentDay[] = $scheduleDays[0];
+
+  function displayMondaySchedule($className)
+  {
+    echo "<div class='class-schema-container'>
+        <div class='$className'><h1 class='klass-namn'>$className</h1></div>
+        <div class='schema'>";
+
+    // open the CSV file
+    if (($csvHandle = fopen("class_schedules/$className.csv", "r")) !== FALSE) {
+      $currentDay = array();
+      while (($scheduleDays = fgetcsv($csvHandle, 1000, ",")) !== FALSE) {
+        $currentDay[] = $scheduleDays[0];
+      }
+
+      foreach ($currentDay as $day) {
+        echo "$day <br>";
+      }
+
+      fclose($csvHandle);
+    } else {
+      echo "Unable to open the CSV file.";
     }
 
-    foreach ($currentDay as $day) {
-      echo "$day <br>";
-    }
-
-    // echo $currentDay[0];
-    // $timeAndClass = $currentDay[1];
-    // echo $timeAndClass;
-
-    fclose($csvHandle);
-  } else {
-    echo "Unable to open the CSV file.";
-  }
-
-  echo "</div>
+    echo "</div>
       </div>";
+  }
+  echo "<div class='year-container'>";
+  displayMondaySchedule('EE22');
+  displayMondaySchedule('ES22');
+  displayMondaySchedule('TE22');
   echo "</div>";
 
   ?>
